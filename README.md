e0b086ea2856e18acafa6cd55b343a4752529211
srsexton94 <srsexton94@gmail.com>
2025-02-18 14:46:13 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Popcorn Pal
+
+This is a movie application built in React with TypeScript, initialized with Vite.
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Popcorn Pal</h1>
+      <p>A movie application</p>
+    </>
+  )
+}
+
+export default App
